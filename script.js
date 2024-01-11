b13848d474642c87e43f90b2c66a0514a06043b3
Sanelma B <92818820+jackiebeeh@users.noreply.github.com>
2024-01-12 00:00:48 +0200

    basic js and style
    size of the "device" needs to be changed to be a fixed size and cells to strech out to full width and height

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 // Script for Etch A Sketch
 
+// Making grid
+
+function makeGrid() {
+    let grid = document.querySelector(".grid")
+    for (let i = 0; i < 16; i++) {
+        let newColumn = document.createElement("div");
+        newColumn.classList.add("column");
+        newColumn.id = "column" + i;
+        grid.appendChild(newColumn);
+        for (let j = 0; j < 16; j++) {
+            let newCell = document.createElement("div");
+            newCell.classList.add("cell");
+            newColumn.appendChild(newCell);
+        }
+    }
+}
+
+window.onload = makeGrid();
+
+let cells = document.querySelectorAll(".cell");
+
+cells.forEach((x) => {
+    x.addEventListener("mouseenter", () => {
+        x.classList.add("draw");
+    });
+    x.addEventListener("mouseleave", () => {
+        setTimeout(() => { 
+            x.classList.remove("draw");
+        }, 3000); 
+    })
+});
