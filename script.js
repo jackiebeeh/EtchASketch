59226a8ae18b1db911ca951f6f2c7c5a210bab9c
Sanelma B <92818820+jackiebeeh@users.noreply.github.com>
2024-01-12 03:51:04 +0200

    Added js to fill grid according to input value.

--- script.js
+++ script.js
@@ -1,41 +1,56 @@
 // Script for Etch A Sketch
 
+// Slider
+let slider = document.getElementById("myRange");
+let output = document.getElementById("output");
+let currentValue = slider.value;
+output.innerHTML = currentValue;
+slider.oninput = function() {
+    console.log(`input value`);
+    currentValue = this.value
+    output.innerHTML = this.value;
+    let grid = document.querySelector(".grid")
+    grid.innerHTML = "";
+    makeGrid();
+}
+
+window.onload = makeGrid();
+
 // Making grid
 function makeGrid() {
     let grid = document.querySelector(".grid")
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < currentValue; i++) {
         let newColumn = document.createElement("div");
         newColumn.classList.add("column");
         newColumn.id = "column" + i;
         grid.appendChild(newColumn);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < currentValue; j++) {
             let newCell = document.createElement("div");
             newCell.classList.add("cell");
             newColumn.appendChild(newCell);
         }
     }
 }
-window.onload = makeGrid();
 
-// Draw
-let cells = document.querySelectorAll(".cell");
-cells.forEach((x) => {
+let device = document.querySelector(".device");
+device.addEventListener("mouseover", () => {
+    let cells = document.querySelectorAll(".cell");
+    cells.forEach((x) => {
     x.addEventListener("mouseenter", () => {
         x.classList.add("draw");
     });
     x.addEventListener("mouseleave", () => {
         setTimeout(() => { 
             x.classList.remove("draw");
-        }, 3000); 
-    })
+        }, 3000);})
+    });
 });
+// Draw
 
-// Slider
 
-let slider = document.getElementById("myRange");
-let output = document.getElementById("output");
-output.innerHTML = slider.value;
 
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
+
+
+
+
+
