2826cbf9c9fa3f2aacd3dedc55c568eef2928981
Sanelma B <92818820+jackiebeeh@users.noreply.github.com>
2024-02-13 09:41:49 +0200

    Fixed the ShakeToErase function

--- script.js
+++ script.js
@@ -145,39 +145,51 @@ window.onload = makeGrid();
 // Shake to erase
 let device = document.querySelector(".device");
 let offsetX, offsetY;
-let initialTop, initialLeft;
+let initialTop = device.style.top;
+let initialLeft = device.style.left;
+let shakeZone = document.querySelector(".shakeZone");
+let cells = document.querySelectorAll(".cell");
+let mousedown = 0;
+let shakeCount = 0;
 
-const move = (e) => {
+device.addEventListener("mousedown", (e) => {
+    mousedown = 1;
+    console.log(mousedown);
+    shakeCount = 0;
+    offsetX = e.clientX - device.offsetLeft;
+    offsetY = e.clientY - device.offsetTop;
+    document.addEventListener("mousemove", move);
+    shakeZoneCounter();
+});
+
+document.addEventListener("mouseup", () => {
+    mousedown = 0;
+    device.style.top = initialTop;
+    device.style.left = initialLeft;
+    console.log(mousedown);
+    document.removeEventListener("mousemove", move);
+})
+
+function move(e) {
     device.style.left = `${e.clientX - offsetX}px`;
     device.style.top = `${e.clientY - offsetY}px`;
 }
 
-device.addEventListener("mousedown", (e) => {
-    let shakeCount = 0;
-    let shakeZone = document.querySelector(".shakeZone");
-    let cells = document.querySelectorAll(".cell");
-
-    initialTop = device.style.top;
-    initialLeft = device.style.left;
-    offsetX = e.clientX - device.offsetLeft;
-    offsetY = e.clientY - device.offsetTop;
-    document.addEventListener("mousemove", move);
-
+function shakeZoneCounter() {
     shakeZone.addEventListener("mouseenter", () => {
-        while (shakeCount < 20) {
+        console.log(`shakeZone mouse entered + shakeCount = ${shakeCount}`);
+        if (shakeCount < 20 && mousedown === 1) {
             shakeCount += 1;
             cells.forEach((cell) => {
                 let bgColor = cell.style.backgroundColor;
-                let shade = "rgb(156, 156, 156)";
-                let percentage = "0.3"
-                let eraseColor = blendRGBColors(bgColor, shade, percentage);
-                cell.style.backgroundColor = eraseColor;   
+                cell.style.backgroundColor = eraseColor(bgColor);   
             });
         }
     });
-});
-window.addEventListener("mouseup", () => {
-    device.style.top = initialTop;
-    device.style.left = initialLeft;
-    document.removeEventListener("mousemove", move);
-});
+}
+
+function eraseColor(bgColor) {
+    let shade = "rgb(156, 156, 156)";
+    let percentage = "0.25";
+    return blendRGBColors(bgColor, shade, percentage);
+}
